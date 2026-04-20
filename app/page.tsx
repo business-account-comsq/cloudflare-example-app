da77b63b7d0df6832e2c6e7df61e07a2e055cbff
business-account-comsq <mizutani_haruto@comsq.com>
2026-04-20 17:48:15 +0900

    fix force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import MessageForm from "@/components/message-form";
 import { getMessagesFromDatabase } from "@/actions/get-message-from-database";
 
 export const runtime="edge"
-
+export const dynamic = "force-dynamic";
 export default async function Home() {
   console.log("① page start");
   let messages = null;

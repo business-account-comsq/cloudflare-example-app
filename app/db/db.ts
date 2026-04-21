cda217fa359e9f506a099f6ca52ec83071e92b0b
business-account-comsq <mizutani_haruto@comsq.com>
2026-04-21 15:41:52 +0900

    delete db-web

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,5 +1,5 @@
-import { drizzle } from "drizzle-orm/libsql/web";
-import { createClient } from "@libsql/client/web";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
 
 export function getDb(env: {
   DATABASE_URL: string;
